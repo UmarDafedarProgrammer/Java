a01609eec6e64d548da5b3fa06cd74123f4241a3
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-03-18 05:04:02 +0530

    Create DefaultValuesForPrimitiveDataTypes.java

---
+++ 1.0.DataTypes/DefaultValuesForPrimitiveDataTypes.java
@@ -0,0 +1,36 @@
+/* Default Values*/
+
+package dataTypes;
+
+public class DataTypes {
+	
+	static int i; // 4 bytes
+	static byte by; // 1 byte
+	static boolean b; // 1 bit
+	static char c; // 2 bytes
+	static short s; // 2 bytes
+	static long ll; // Long - L/l 8 Bytes
+	static float f; // Float 4 bytes
+	static double d; // 8 bytes
+
+	public static void main(String[] args) {
+		//Declaring the variables of Primitive Data Types
+		
+		System.out.println("boolean b holds the value "+b);
+		System.out.println("byte by holds the value "+by);
+		System.out.println("Character c holds the value "+c);
+		System.out.println("short s holds the value "+s);
+		System.out.println("Integer i holds the value "+i);
+		System.out.println("Long ll holds the value "+ll);
+		System.out.println("float f holds the value "+f);
+		System.out.println("double d holds the value "+d);
+		
+	}
+
+}
+
+
+/*
+  OUTPUT:
+  
+*/
