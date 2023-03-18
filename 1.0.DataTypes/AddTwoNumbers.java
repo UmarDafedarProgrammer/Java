b3221151aaa809b4552c845f0ae684141666032f
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-03-18 05:54:32 +0530

    Create AddTwoNumbers.java

---
+++ 1.0.DataTypes/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package dataTypes;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a1,a2,sum;
+		
+		a1=10;
+		a2=30;
+		sum=a1+a2;
+		System.out.println("Sum of "+a1+" and "+a2 +" is "+sum);
+	}
+
+}
